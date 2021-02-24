71a5f35037a0e1ba345a088b01092bdbba943a5f
Hawkpath <hawkpathas@gmail.com>
2021-02-24 03:54:49 -0500

    Fix inconsistent indentation.

--- memebot/renderer/src/message_generator.js
+++ memebot/renderer/src/message_generator.js
@@ -3,31 +3,31 @@
 var dayjs = require('../node_modules/dayjs');
 
 function createFragment(htmlString) {
-  return document.createRange().createContextualFragment(htmlString);
+    return document.createRange().createContextualFragment(htmlString);
 }
 
 function getContentString(messageContent) {
-  return `<div class="markup-2BOw-j messageContent-2qWWxC">${messageContent}</div>`;
+    return `<div class="markup-2BOw-j messageContent-2qWWxC">${messageContent}</div>`;
 }
 
 function addGroupStart(parent, authorName, authorColor, authorAvatarUrl, timestamp, messageContent) {
-  var timeString = transformTimestamp(timestamp);
-  parent.add(createFragment(`
-    <div class="message-2qnXI6 cozyMessage-3V1Y8y groupStart-23k01U wrapper-2a6GCs cozy-3raOZG zalgo-jN1Ica">
-        <div class="contents-2mQqc9"><img src="${authorAvatarUrl}" class="avatar-1BDn8e clickable-1bVtEA">
-            <h2 class="header-23xsNx"><span class="headerText-3Uvj1Y"><span class="username-1A8OIy clickable-1bVtEA" style="color: ${authorColor};">${authorName}</span></span><span class="timestamp-3ZCmNB">${timeString}</span></h2>
-            ${getContentString(messageContent)}
+    var timeString = transformTimestamp(timestamp);
+    parent.add(createFragment(`
+        <div class="message-2qnXI6 cozyMessage-3V1Y8y groupStart-23k01U wrapper-2a6GCs cozy-3raOZG zalgo-jN1Ica">
+            <div class="contents-2mQqc9"><img src="${authorAvatarUrl}" class="avatar-1BDn8e clickable-1bVtEA">
+                <h2 class="header-23xsNx"><span class="headerText-3Uvj1Y"><span class="username-1A8OIy clickable-1bVtEA" style="color: ${authorColor};">${authorName}</span></span><span class="timestamp-3ZCmNB">${timeString}</span></h2>
+                ${getContentString(messageContent)}
+            </div>
         </div>
-    </div>
-  `));
+    `));
 }
 
 function addMessage(parent, messageContent) {
-  parent.add(createFragment(`
-    <div class="message-2qnXI6 cozyMessage-3V1Y8y wrapper-2a6GCs cozy-3raOZG zalgo-jN1Ica">
-        ${getContentString(messageContent)}
-    </div>
-  `));
+    parent.add(createFragment(`
+        <div class="message-2qnXI6 cozyMessage-3V1Y8y wrapper-2a6GCs cozy-3raOZG zalgo-jN1Ica">
+            ${getContentString(messageContent)}
+        </div>
+    `));
 }
 
 function transformTimestamp(timestamp) {
