0fe10c6144c72184be6cb65fa954530a6a279772
Hawkpath <hawkpathas@gmail.com>
2021-02-24 05:07:16 -0500

    Fix using wrong method names.

--- memebot/renderer/src/message_generator.js
+++ memebot/renderer/src/message_generator.js
@@ -10,7 +10,7 @@ function getContentString(messageContent) {
 
 function addGroupStart(parent, authorName, authorColor, authorAvatarUrl, timestamp, messageContent) {
     const timeString = transformTimestamp(timestamp);
-    parent.add(createFragment(`
+    parent.appendChild(createFragment(`
         <div class="message-2qnXI6 cozyMessage-3V1Y8y groupStart-23k01U wrapper-2a6GCs cozy-3raOZG zalgo-jN1Ica">
             <div class="contents-2mQqc9"><img src="${authorAvatarUrl}" class="avatar-1BDn8e clickable-1bVtEA">
                 <h2 class="header-23xsNx"><span class="headerText-3Uvj1Y"><span class="username-1A8OIy clickable-1bVtEA" style="color: ${authorColor};">${authorName}</span></span><span class="timestamp-3ZCmNB">${timeString}</span></h2>
@@ -21,7 +21,7 @@ function addGroupStart(parent, authorName, authorColor, authorAvatarUrl, timesta
 }
 
 function addMessage(parent, messageContent) {
-    parent.add(createFragment(`
+    parent.appendChild(createFragment(`
         <div class="message-2qnXI6 cozyMessage-3V1Y8y wrapper-2a6GCs cozy-3raOZG zalgo-jN1Ica">
             ${getContentString(messageContent)}
         </div>
