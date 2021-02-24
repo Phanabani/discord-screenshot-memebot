f431535207a91c15088bbaaac7718e21001f8686
Hawkpath <hawkpathas@gmail.com>
2021-02-24 03:46:28 -0500

    Use camelCase, silly!

--- memebot/renderer/src/message_generator.js
+++ memebot/renderer/src/message_generator.js
@@ -2,29 +2,30 @@
 
 var dayjs = require('../node_modules/dayjs');
 
-function create_fragment(html_string) {
-  return document.createRange().createContextualFragment(html_string);
+function createFragment(htmlString) {
+  return document.createRange().createContextualFragment(htmlString);
 }
 
-function get_content_string(message_content) {
-  return `<div class="markup-2BOw-j messageContent-2qWWxC">${message_content}</div>`;
+function getContentString(messageContent) {
+  return `<div class="markup-2BOw-j messageContent-2qWWxC">${messageContent}</div>`;
 }
 
-function add_group_start(parent, author_name, author_color, author_avatar_url, time_string, message_content) {
-  parent.add(create_fragment(`
+function addGroupStart(parent, authorName, authorColor, authorAvatarUrl, timestamp, messageContent) {
+  var timeString = transformTimestamp(timestamp);
+  parent.add(createFragment(`
     <div class="message-2qnXI6 cozyMessage-3V1Y8y groupStart-23k01U wrapper-2a6GCs cozy-3raOZG zalgo-jN1Ica">
-        <div class="contents-2mQqc9"><img src="${author_avatar_url}" class="avatar-1BDn8e clickable-1bVtEA">
-            <h2 class="header-23xsNx"><span class="headerText-3Uvj1Y"><span class="username-1A8OIy clickable-1bVtEA" style="color: ${author_color};">${author_name}</span></span><span class="timestamp-3ZCmNB">${time_string}</span></h2>
-            ${get_content_string(message_content)}
+        <div class="contents-2mQqc9"><img src="${authorAvatarUrl}" class="avatar-1BDn8e clickable-1bVtEA">
+            <h2 class="header-23xsNx"><span class="headerText-3Uvj1Y"><span class="username-1A8OIy clickable-1bVtEA" style="color: ${authorColor};">${authorName}</span></span><span class="timestamp-3ZCmNB">${timeString}</span></h2>
+            ${getContentString(messageContent)}
         </div>
     </div>
   `));
 }
 
-function add_message(parent, message_content) {
-  parent.add(create_fragment(`
+function addMessage(parent, messageContent) {
+  parent.add(createFragment(`
     <div class="message-2qnXI6 cozyMessage-3V1Y8y wrapper-2a6GCs cozy-3raOZG zalgo-jN1Ica">
-        ${get_content_string(message_content)}
+        ${getContentString(messageContent)}
     </div>
   `));
 }
